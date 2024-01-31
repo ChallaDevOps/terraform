8c28e6a00ce9d6a10be51859d69ebba3b5b4c3f6
ChallaDevOps <146905925+ChallaDevOps@users.noreply.github.com>
2024-01-31 07:08:17 +0530

    Add files via upload

---
+++ prod-vpc/vpc.tf
@@ -0,0 +1,110 @@
+resource "aws_vpc" "charani" {
+  cidr_block       = "192.168.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Production VPC"
+  }
+}
+
+resource "aws_subnet" "pub-sn" {
+    vpc_id = aws_vpc.charani.id
+    cidr_block = "192.168.1.0/24"
+    availability_zone = "ap-south-1a"
+
+    tags = {
+      Name = "Public SN"
+    } 
+}
+
+
+resource "aws_subnet" "pri-sn" {
+    vpc_id = aws_vpc.charani.id
+    cidr_block = "192.168.2.0/24"
+    availability_zone = "ap-south-1a"
+
+    tags = {
+      Name = "Private SN"
+    } 
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.charani.id
+
+  tags = {
+    Name = "Prod-IGW"
+  }
+}
+
+resource "aws_eip" "lb" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.lb.id
+  subnet_id     = aws_subnet.pri-sn.id
+
+  tags = {
+    Name = "Prod NAT Gateway"
+  }
+
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
+#   depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_route_table" "pub-rt" {
+  vpc_id = aws_vpc.charani.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Prod public rt"
+  }
+}
+
+resource "aws_route_table" "pri-rt" {
+  vpc_id = aws_vpc.charani.id
+
+  route {
+    cidr_block = "192.168.2.0/24"
+    gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = "Prod private rt"
+  }
+}
+
+
+
+
+resource "aws_security_group" "charani-sg" {
+  name = "Prod VPC SG"
+  description = "SG for Prod VPC"
+  vpc_id = aws_vpc.charani.id
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks      = [aws_vpc.charani.cidr_block]
+  }
+   ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks      = [aws_vpc.charani.cidr_block]
+  }
+}
+
+
